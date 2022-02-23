6bf337b23c438b6e122be8e5c9b5a895707091b7
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-02-23 18:26:36 +0100

    Some cleanup

--- hosts/proteus/home.nix
+++ hosts/proteus/home.nix
@@ -1,28 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "ap";
   home.homeDirectory = "/home/ap";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "21.11";
 
   home.packages = with pkgs; [
-  # Fonts
-  (nerdfonts.override { fonts = [ "Iosevka" "Overpass"]; })
+  (nerdfonts.override { fonts = [ "Iosevka" "Overpass" ]; })
 
   # GUI Apps
   unstable.mailspring
@@ -394,7 +380,7 @@
     alias ls='exa -al --color=always --group-directories-first' # my preferred listing
 
     alias js='cd /home/ap/Coding/javascript'
-    alias py='cd /home/ap/Coding/python'
+    alias py='cd /home/ap/Coding/python/recent'
     alias rs='cd /home/ap/Coding/rust'
     alias cpp='cd /home/ap/Coding/cpp'
     alias ans='cd /home/ap/Coding/ansible'
