797d17914171914240a43c96722704e25c1ff860
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-03-08 20:47:21 +0100

    Update home.nix

--- hosts/proteus/home.nix
+++ hosts/proteus/home.nix
@@ -386,4 +386,16 @@
     alias ans='cd /home/ap/Coding/ansible'
     '';
   };
+
+  xdg.userDirs = {
+        enable = true;
+        documents = "$HOME/misc/";
+        download = "$HOME/Downloads/";
+        videos = "$HOME/misc/";
+        music = "$HOME/misc/";
+        pictures = "$HOME/Pictures/";
+        desktop = "$HOME/misc/";
+        publicShare = "$HOME/misc/";
+        templates = "$HOME/misc/";
+    };
 }
