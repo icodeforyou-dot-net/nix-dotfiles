916cd41f7d8ea7908e3b1549d2d8b3f33d49c768
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-04-23 14:58:19 +0200

    Update configuration-kernel-5.17.4.nix

--- hosts/archon/configuration-kernel-5.17.4.nix
+++ hosts/archon/configuration-kernel-5.17.4.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_16.override {
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_17.override {
     argsOverride = rec {
       src = pkgs.fetchurl {
             url = "https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
