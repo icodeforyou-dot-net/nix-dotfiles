04f1546fe6f26cb45deefadac37ac4ac8083df26
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-04-23 13:51:33 +0200

    Create configuration-kernel-5.16.10.nix

---
+++ hosts/archon/configuration-kernel-5.16.10.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_16.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "17i3j07hgljsiz2kymbskp35p2xp14gb0mdi5s2r61c0h406yk8c";
+      };
+      version = "5.16.10";
+      modDirVersion = "5.16.10";
+      };
+  });
+}
