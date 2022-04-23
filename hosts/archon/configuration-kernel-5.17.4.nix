5489c96899f899438b0e333b112197d8ac11629e
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-04-23 14:55:54 +0200

    Create configuration-kernel-5.17.4.nix

---
+++ hosts/archon/configuration-kernel-5.17.4.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_16.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "1ifkl1j5dimipqxwki26i4v6gav70g24456y7ynbb71sx1pdag3f";
+      };
+      version = "5.17.4";
+      modDirVersion = "5.17.4";
+      };
+  });
+}
