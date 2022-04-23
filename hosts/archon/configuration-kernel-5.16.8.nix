7c28a410baf9b12413abdc2ab839e7a891ccbb86
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-04-23 11:38:33 +0200

    Create configuration-kernel-5.16.8.nix

---
+++ hosts/archon/configuration-kernel-5.16.8.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_16.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "05h3b11czr710lilmb5gq84a78cfz3jm03q2q0gcrpcaxq2mzajj";
+      };
+      version = "5.16.8";
+      modDirVersion = "5.16.8";
+      };
+  });
+}
