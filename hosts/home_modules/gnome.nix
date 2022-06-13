dea1fa80c8b3344d30b1d019149c38b5d61fc560
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-06-13 10:08:44 +0200

    Update gnome.nix

--- hosts/home_modules/gnome.nix
+++ hosts/home_modules/gnome.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
   gnome.gnome-tweaks
   gnome.networkmanager-openvpn
-  gnome-terminal
+  gnome.gnome-terminal
   unstable.gnomeExtensions.dash-to-dock
   gnomeExtensions.tweaks-in-system-menu
   gnomeExtensions.gnome-vagrant-indicator
