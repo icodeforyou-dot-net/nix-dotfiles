898501ba40b7a2cb453747b82656c55ec2cf9f1d
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-07-05 10:10:21 +0200

    Update cli-os.nix

--- hosts/home_modules/cli-os.nix
+++ hosts/home_modules/cli-os.nix
@@ -20,6 +20,7 @@
   universal-ctags
   du-dust
   duf
+  tree-sitter
 
   awscli
   kaggle
@@ -45,6 +46,11 @@
 
   programs.bash = {
     enable = true;
+    profileExtra = ''
+    export env WAYLAND_DISPLAY= alacritty
+    export MOZ_ENABLE_WAYLAND=1
+    export MOZ_USE_XINPUT2=1
+    '';
     initExtra = ''
     eval "$(direnv hook bash)"
     eval "$(zoxide init bash)"
@@ -76,24 +82,24 @@
       };
     };
 
-  # programs.zellij = {
-  #     enable = true;
-  #     settings = {
-  #       theme = "nord";
-  #       themes.nord = {
-  #         fg = "[216, 222, 233] #D8DEE9";
-  #         bg = "[46, 52, 64] #2E3440";
-  #         black = "[59, 66, 82] #3B4252";
-  #         red = "[191, 97, 106] #BF616A";
-  #         green = "[163, 190, 140] #A3BE8C";
-  #         yellow = "[235,203,139] #EBCB8B";
-  #         blue = "[129, 161, 193] #81A1C1";
-  #         magenta = "[180, 142, 173] #B48EAD";
-  #         cyan = "[136, 192, 208] #88C0D0";
-  #         white = "[229, 233, 240] #E5E9F0";
-  #         orange = "[208, 135, 112] #D08770";
-  #       };
-  #     };
-  # };
+  programs.zellij = {
+      enable = true;
+      settings = {
+        theme = "nord";
+        themes.nord = {
+          fg = "[216, 222, 233] #D8DEE9";
+          bg = "[46, 52, 64] #2E3440";
+          black = "[59, 66, 82] #3B4252";
+          red = "[191, 97, 106] #BF616A";
+          green = "[163, 190, 140] #A3BE8C";
+          yellow = "[235,203,139] #EBCB8B";
+          blue = "[129, 161, 193] #81A1C1";
+          magenta = "[180, 142, 173] #B48EAD";
+          cyan = "[136, 192, 208] #88C0D0";
+          white = "[229, 233, 240] #E5E9F0";
+          orange = "[208, 135, 112] #D08770";
+        };
+      };
+  };
 
 }
