9e017a20f1bf5fd61182ff8de809afc450d6acd5
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-07-10 20:50:07 +0200

    Create wm-hyprland.nix

---
+++ hosts/home_modules/wm-hyprland.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Foot wayland terminal
+    unstable.foot
+    # hyprland compositor
+    unstable.hyprland
+    # terminal file manager
+    unstable.lf
+    # Wofi wayland launcher
+    unstable.wofi
+    # wayland based logout menu
+    unstable.wlogout
+    # Xfce file manager
+    unstable.xfce.thunar
+  ];
+
+  # Mako, lightweight notification daemon for Wayland
+  programs.mako = {
+    enable = true;
+  };
+
+  # Waybar
+  programs.waybar = {
+    enable = true;
+  };
+}
