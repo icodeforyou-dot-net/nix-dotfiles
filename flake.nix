776cf53506d915deab2807bfb2ac080fc94effcc
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-07-28 17:46:07 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,27 +10,27 @@
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     # hyprland = {
     #   url = "github:hyprwm/Hyprland";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, home-manager, hyprland, ... }@inputs: 
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
-      
+
       pkgs = import nixpkgs {
         inherit system;
-	      config = { 
+        config = {
           allowUnfree = true;
         };
       };
 
       pkgs-unstable = import nixpkgs-unstable {
         inherit system;
-	      config = { 
+        config = {
           allowUnfree = true;
         };
       };
@@ -38,87 +38,87 @@
       lib = nixpkgs.lib;
 
       overlay-unstable = final: prev: {
-          unstable = import nixpkgs-unstable {
+        unstable = import nixpkgs-unstable {
           system = "x86_64-linux";
           config.allowUnfree = true;
-          };
+        };
       };
 
 
-    in 
+    in
     {
-     nixosConfigurations = {
+      nixosConfigurations = {
 
-      proteus = lib.nixosSystem {
-        inherit system;
-	      modules = [ 
-          ./hosts/shared_configuration.nix
-	        ./hosts/proteus/configuration.nix
-          ./hosts/system_modules/pipewire.nix
-          { nix.registry.nixpkgs.flake = nixpkgs; }
-          { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
-	      ];
-       };
-
-      archon = lib.nixosSystem {
-        inherit system;
-        modules = [ 
-          ./hosts/shared_configuration.nix
-	        ./hosts/archon/configuration.nix
-          ./hosts/kernels/kernel-5.16.9.nix
-          { nix.registry.nixpkgs.flake = nixpkgs; }
-          { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
-	      ];
-       };
-
-     };
-
-     homeConfigurations = {
-       ap-proteus = home-manager.lib.homeManagerConfiguration {
-        inherit system pkgs;
-	      username = "ap";
-	      homeDirectory = "/home/ap";
-	      stateVersion = "22.05";
-	      configuration = {
-
-          nixpkgs.config = { allowUnfree = true; };
-          nixpkgs.overlays = [ overlay-unstable ];
-	   
-          imports = [
-            ./hosts/shared_home.nix
-            ./hosts/home_modules/cli-os.nix
-            ./hosts/home_modules/coding.nix
-            ./hosts/home_modules/fonts.nix
-            ./hosts/home_modules/gnome.nix
-            ./hosts/home_modules/gui.nix   
-            ./hosts/proteus/home.nix
+        proteus = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/shared_configuration.nix
+            ./hosts/proteus/configuration.nix
+            ./hosts/system_modules/pipewire.nix
+            { nix.registry.nixpkgs.flake = nixpkgs; }
+            { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
           ];
-	        };
-       };
-       ap-archon = home-manager.lib.homeManagerConfiguration {
-        inherit system pkgs;
-        username = "ap";
-        homeDirectory = "/home/ap";
-        stateVersion = "22.05";
-        configuration = {
-  
-          nixpkgs.config = { allowUnfree = true; };
-          nixpkgs.overlays = [ overlay-unstable ];
-      
-          imports = [
-            ./hosts/shared_home.nix
-            ./hosts/home_modules/audio.nix
-            ./hosts/home_modules/cli-os.nix
-            ./hosts/home_modules/coding.nix
-            ./hosts/home_modules/fonts.nix
-            ./hosts/home_modules/gaming/dosbox.nix
-            ./hosts/home_modules/gnome.nix
-            ./hosts/home_modules/gui.nix            
-            ./hosts/archon/home.nix
+        };
+
+        archon = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/shared_configuration.nix
+            ./hosts/archon/configuration.nix
+            ./hosts/kernels/kernel-5.16.9.nix
+            { nix.registry.nixpkgs.flake = nixpkgs; }
+            { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
           ];
+        };
+
+      };
+
+      homeConfigurations = {
+        ap-proteus = home-manager.lib.homeManagerConfiguration {
+          inherit system pkgs;
+          username = "ap";
+          homeDirectory = "/home/ap";
+          stateVersion = "22.05";
+          configuration = {
+
+            nixpkgs.config = { allowUnfree = true; };
+            nixpkgs.overlays = [ overlay-unstable ];
+
+            imports = [
+              ./hosts/shared_home.nix
+              ./hosts/home_modules/cli-os.nix
+              ./hosts/home_modules/coding.nix
+              ./hosts/home_modules/fonts.nix
+              ./hosts/home_modules/gnome.nix
+              ./hosts/home_modules/gui.nix
+              ./hosts/proteus/home.nix
+            ];
           };
-     };
-    };
+        };
+        ap-archon = home-manager.lib.homeManagerConfiguration {
+          inherit system pkgs;
+          username = "ap";
+          homeDirectory = "/home/ap";
+          stateVersion = "22.05";
+          configuration = {
+
+            nixpkgs.config = { allowUnfree = true; };
+            nixpkgs.overlays = [ overlay-unstable ];
+
+            imports = [
+              ./hosts/shared_home.nix
+              ./hosts/home_modules/audio.nix
+              ./hosts/home_modules/cli-os.nix
+              ./hosts/home_modules/coding.nix
+              ./hosts/home_modules/fonts.nix
+              ./hosts/home_modules/gaming/dosbox.nix
+              ./hosts/home_modules/gnome.nix
+              ./hosts/home_modules/gui.nix
+              ./hosts/archon/home.nix
+            ];
+          };
+        };
+      };
 
     };
 }
