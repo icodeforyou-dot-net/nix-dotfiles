f504d1be12756d7cfd26b191ec8f18d9279eedfa
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-07-28 18:03:42 +0200

    Add override for tutanota desktop

--- hosts/home_modules/gui.nix
+++ hosts/home_modules/gui.nix
@@ -1,41 +1,54 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nixpkgs, ... }:
 
+let
+  tutanota-desktop-override = with pkgs; (unstable.tutanota-desktop.overrideAttrs
+    (oldAttrs: rec {
+      pname = "tutanota-desktop";
+      version = "3.98.15";
+      src = fetchurl {
+        url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
+        name = "tutanota-desktop-${version}.tar.gz";
+        sha256 = "sha256-yuSFDLQ7ckuZYuXREbW0VvMapzO2VhVIH1Ccr1uQHmo=";
+      };
+    }));
+in
 {
   home.packages = with pkgs; [
 
-  unstable.mailspring
-  slack
-  unstable.discord
-  unstable.element-desktop
-  skypeforlinux
-  unstable.bitwarden
-  unstable.signal-desktop
-  unstable.tdesktop
-  unstable.whatsapp-for-linux
-  insomnia
-  vlc
-  gimp-with-plugins
-  darktable
-  inkscape
-  pdfarranger
-  unstable.onlyoffice-bin
-  zoom-us
-  unstable.tutanota-desktop
-  calibre
-  unstable.obsidian
+    unstable.mailspring
+    slack
+    unstable.discord
+    unstable.element-desktop
+    skypeforlinux
+    unstable.bitwarden
+    unstable.signal-desktop
+    unstable.tdesktop
+    unstable.whatsapp-for-linux
+    insomnia
+    vlc
+    gimp-with-plugins
+    darktable
+    inkscape
+    pdfarranger
+    unstable.onlyoffice-bin
+    zoom-us
+    tutanota-desktop-override
 
-# Handwritng
-  write_stylus
-  xournalpp
+    calibre
+    unstable.obsidian
 
-# Browser
-  chromium
-  brave
-  lagrange
+    # Handwritng
+    write_stylus
+    xournalpp
 
-  # tor
-  unstable.onionshare-gui
-  unstable.onionshare
-  unstable.tor-browser-bundle-bin
-];
+    # Browser
+    chromium
+    brave
+    lagrange
+
+    # tor
+    unstable.onionshare-gui
+    unstable.onionshare
+    unstable.tor-browser-bundle-bin
+  ];
 }
