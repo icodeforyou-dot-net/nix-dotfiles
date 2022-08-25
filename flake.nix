5c701ab904624197291a79f2213a5936c0e00bc0
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-08-25 14:24:38 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
 
             imports = [
               ./hosts/shared_home.nix
-              ./hosts/games/opemmw.nix
+              ./hosts/home_modules/games/openmw.nix
               ./hosts/home_modules/audio.nix
               ./hosts/home_modules/cli-os.nix
               ./hosts/home_modules/coding.nix
