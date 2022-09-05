6879cd4e446c7dea47387bd5abfecbc1c3da95a1
ap@icodeforyou.net <andreas-p@tutanota.com>
2022-09-05 11:09:38 +0200

    Switch tutanota desktop back to upstream version

--- hosts/perseus/modules/gui.nix
+++ hosts/perseus/modules/gui.nix
@@ -1,16 +1,16 @@
 { config, pkgs, nixpkgs, ... }:
 
 let
-  tutanota-desktop-override = with pkgs; (unstable.tutanota-desktop.overrideAttrs
-    (oldAttrs: rec {
-      pname = "tutanota-desktop";
-      version = "3.98.15";
-      src = fetchurl {
-        url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
-        name = "tutanota-desktop-${version}.tar.gz";
-        sha256 = "sha256-yuSFDLQ7ckuZYuXREbW0VvMapzO2VhVIH1Ccr1uQHmo=";
-      };
-    }));
+  # tutanota-desktop-override = with pkgs; (unstable.tutanota-desktop.overrideAttrs
+  #   (oldAttrs: rec {
+  #     pname = "tutanota-desktop";
+  #     version = "3.98.15";
+  #     src = fetchurl {
+  #       url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
+  #       name = "tutanota-desktop-${version}.tar.gz";
+  #       sha256 = "sha256-yuSFDLQ7ckuZYuXREbW0VvMapzO2VhVIH1Ccr1uQHmo=";
+  #     };
+  #   }));
 in
 {
   home.packages = with pkgs; [
@@ -26,13 +26,14 @@ in
     insomnia
     vlc
     darktable
-    tutanota-desktop-override
+    unstable.tutanota-desktop
 
     calibre
 
     # Browser
     chromium
-    brave
+    unstable.brave
+    unstable.firefox
 
     # tor
     unstable.onionshare-gui
