debbc842b814bf7b6f046a62476fa8cb03391d86
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-09-05 19:08:31 +0200

    Update gui.nix

--- hosts/perseus/modules/gui.nix
+++ hosts/perseus/modules/gui.nix
@@ -11,43 +11,72 @@ let
   #       sha256 = "sha256-yuSFDLQ7ckuZYuXREbW0VvMapzO2VhVIH1Ccr1uQHmo=";
   #     };
   #   }));
+  wrapped-chromium = with pkgs; (writeShellScriptBin "chromium" ''
+    exec firejail ${pkgs.chromium}/bin/chromium
+  '');
+
+  wrapped-discord = with pkgs; (writeShellScriptBin "discord" ''
+    exec firejail ${pkgs.chromium}/bin/discord
+  '');
+
+  wrapped-firefox = with pkgs; (writeShellScriptBin "firefox" ''
+    exec firejail ${pkgs.firefox}/bin/firefox
+  '');
 in
 {
   home.packages = with pkgs; [
 
-    unstable.mailspring
-    discord
-    unstable.element-desktop
-    skypeforlinux
     unstable.bitwarden
+    darktable
+    
+    # Creating a wrapper for discord to run it in firejail
+    (symlinkJoin {
+      name = "discord";
+      paths = [
+        wrapped-discord
+        pkgs.discord
+        ];
+    })
+
+    unstable.element-desktop
+    unstable.mailspring
+
+
+    skypeforlinux
+
     unstable.signal-desktop
     unstable.tdesktop
     unstable.whatsapp-for-linux
     insomnia
     vlc
-    darktable
     unstable.tutanota-desktop
 
     calibre
 
     # Browser
-    chromium
     unstable.brave
+
+    # Creating a wrapper for chromium to run it in firejail
+    (symlinkJoin {
+      name = "chromium";
+      paths = [
+        wrapped-chromium
+        pkgs.chromium
+        ];
+    })
 
     # Creating a wrapper for firefox to run it in firejail
     (symlinkJoin {
-      paths = [ firefox ];
-      inherit (firefox) name pname version;
-      nativeBuildInputs = [ makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/firefox \
-          firejail firefox
-      '';
+      name = "firefox";
+      paths = [
+        wrapped-firefox
+        pkgs.firefox
+        ];
     })
 
     # tor
     unstable.onionshare-gui
     unstable.onionshare
-    unstable.tor-browser-bundle-bin
+    # unstable.tor-browser-bundle-bin
   ];
 }
