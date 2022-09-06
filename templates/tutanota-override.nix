5e3b4027f7175b2051782dcf83baf9841a18a6e1
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-09-06 14:03:45 +0200

    Create templates

---
+++ templates/tutanota-override.nix
@@ -0,0 +1,20 @@
+  # Overriding the tutanota package installed via home-manager
+  { config, pkgs, nixpkgs, ... }:
+  
+  let
+  tutanota-desktop-override = with pkgs; (unstable.tutanota-desktop.overrideAttrs
+    (oldAttrs: rec {
+      pname = "tutanota-desktop";
+      version = "3.98.15";
+      src = fetchurl {
+        url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
+        name = "tutanota-desktop-${version}.tar.gz";
+        sha256 = "sha256-yuSFDLQ7ckuZYuXREbW0VvMapzO2VhVIH1Ccr1uQHmo=";
+      };
+    }));
+in
+{
+  home.packages = with pkgs; [
+    tutanota-desktop-override
+  ];
+}
