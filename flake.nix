cec885b2d3670dd03afc1e75f137431cd957b312
icodeforyou-dot-net <andreas-p@tutanota.com>
2022-10-27 21:30:15 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/Hyprland/7a775c0"; #9370c7a
+      url = "github:hyprwm/Hyprland/2c2e35e"; #2c2e35e #7a775c0 #9370c7a
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
