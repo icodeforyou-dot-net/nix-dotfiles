d6fc6f718725b9ad5c206ba1d3fec1a27663f37c
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-04-17 18:17:59 +0200

    Update networking-basic.nix

--- config/sys/modules/networking-basic.nix
+++ config/sys/modules/networking-basic.nix
@@ -2,7 +2,9 @@
 {
   networking = {
     firewall.enable = true;
-    hosts = "192.168.200.100" = [ "minikube.local" ];
+    hosts = {
+      "192.168.200.100" = [ "minikube.local" ];
+    };
     networkmanager.enable = true;
   };
 }
