2896045169dbcd818c028adc1a45e9ff49b32b4a
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-06-05 16:47:17 +0200

    Create separate config for virtualbox

---
+++ config/sys/modules/virtualization-virtualbox.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+  virtualbox = {
+    guest.enable = false;
+    host = {
+      enable = true;
+      enableHardening = true;
+      enableExtensionPack = true;
+    };
+  };
+};
+
+
+}
