6c9e8aab2021289a929ad430ed38c9f5dc266e6a
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-06-05 16:53:09 +0200

    Update flake to NixOS 23.05

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "System Flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
 
     nixpkgs-22-05.url = "nixpkgs/nixos-22.05";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
