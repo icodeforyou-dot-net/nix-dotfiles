82590b91447b43d501dd5413944da0e671d44c2d
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-06-15 16:45:39 +0200

    Add dconf-editor, nautilus-open-any-terminal

--- config/home/modules/gnome.nix
+++ config/home/modules/gnome.nix
@@ -2,11 +2,13 @@
 
 {
   home.packages = with pkgs; [
+    gnome.dconf-editor
     desktop-file-utils
     unstable.gnome.gnome-tweaks
     catppuccin-gtk
     gnome.networkmanager-openvpn
     gnome.gnome-terminal
+    unstable.nautilus-open-any-terminal
     unstable.gnomeExtensions.dash-to-dock
     gnomeExtensions.tweaks-in-system-menu
     tela-icon-theme
