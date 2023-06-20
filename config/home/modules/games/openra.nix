370bf04dd80c2842f107cfab20bf299ff75ec4c4
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-06-20 19:03:56 +0200

    Add back openra

--- config/home/modules/games/openra.nix
+++ config/home/modules/games/openra.nix
@@ -3,9 +3,9 @@
 {
   home.packages = with pkgs; [
     openra
-    unstable.openraPackages.mods.yr
-    unstable.openraPackages.mods.ss
-    unstable.openraPackages.mods.ura
-    unstable.openraPackages.mods.raclassic
+    # unstable.openraPackages.mods.yr
+    # unstable.openraPackages.mods.ss
+    # unstable.openraPackages.mods.ura
+    # unstable.openraPackages.mods.raclassic
   ];
 }
