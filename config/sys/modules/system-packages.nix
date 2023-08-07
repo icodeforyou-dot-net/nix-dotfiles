c434190f861a3f9051ae969be95f780ed67aa3f1
ap@icodeforyou.net <andreas-p@tutanota.com>
2023-08-07 21:05:48 +0200

    Add GSettings overrides for nautilus extensions

--- config/sys/modules/system-packages.nix
+++ config/sys/modules/system-packages.nix
@@ -17,4 +17,15 @@
     gnome.nautilus-python
     nautilus-open-any-terminal
   ];
+
+  # To make nautilus extensions gsettings available
+  services.xserver.desktopManager.gnome.extraGSettingsOverridePackages = with pkgs; [
+    nautilus-open-any-terminal
+  ];
+
+  environment.sessionVariables.NAUTILUS_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
+
+  environment.pathsToLink = [
+    "/share/nautilus-python/extensions"
+  ];
 }
