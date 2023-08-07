8dacdc67bd5abeb91c5fdcb767f51720047f8616
ap@icodeforyou.net <andreas-p@tutanota.com>
2023-08-07 21:06:08 +0200

    Add several gnome apps

--- config/home/modules/gnome.nix
+++ config/home/modules/gnome.nix
@@ -8,6 +8,10 @@
     catppuccin-gtk
     gnome.networkmanager-openvpn
     gnome.gnome-terminal
+    gnome.gnome-boxes
+    gnome.polari
+    warp
+    unstable.amberol
     # don't put nautilus extensions here, see issue https://github.com/NixOS/nixpkgs/issues/126074
     # unstable.nautilus-python
     # unstable.nautilus-open-any-terminal
