8899ba377cb3a6fbab09dfad2a3008836fd336f5
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-08-10 21:50:08 +0200

    Set kubectl version

--- config/home/modules/languages.nix
+++ config/home/modules/languages.nix
@@ -12,83 +12,96 @@ let
   my-helmfile = with pkgs.unstable; helmfile-wrapped.override {
     inherit (my-kubernetes-helm.passthru) pluginsDir;
   };
+
+  kubectl-v1-24-16 = with pkgs.unstable; (kubectl.overrideAttrs (
+    (oldAttrs: rec {
+      version = "1.24.16";
+
+      src = fetchFromGitHub {
+        owner = "kubernetes";
+        repo = "kubernetes";
+        rev = "v${version}";
+        hash = "sha256-rZtcCmDxpAN8zL4D0xGRC6yiEk65fHYADQjFN/HLLJU=";
+      };
+    })
+  ));
+
 in
 
 {
-  home.packages = with pkgs;
-    [
+  home.packages = with pkgs; [
 
-      # general
-      unstable.git
-      #github-desktop
-      gittyup
+    # general
+    unstable.git
+    #github-desktop
+    gittyup
 
-      # C
-      unstable.gcc
+    # C
+    unstable.gcc
 
-      # lua
-      lua
-      unstable.stylua
+    # lua
+    lua
+    unstable.stylua
 
-      # markdown
-      # nodePackages.livedown
-      pandoc
+    # markdown
+    # nodePackages.livedown
+    pandoc
 
-      # Nix
-      nixpkgs-fmt
-      unstable.alejandra
-      dconf2nix
-      rnix-lsp
+    # Nix
+    nixpkgs-fmt
+    unstable.alejandra
+    dconf2nix
+    rnix-lsp
 
-      # python
-      (python3.withPackages (ps: with ps; [ setuptools pip debugpy ]))
+    # python
+    (python3.withPackages (ps: with ps; [ setuptools pip debugpy ]))
 
-      lldb # debugging setup
+    lldb # debugging setup
 
-      # Containers
-      dive
-      skopeo
+    # Containers
+    dive
+    skopeo
 
-      # ansible
-      unstable.ansible
-      unstable.ansible-lint
-      sshpass
+    # ansible
+    unstable.ansible
+    unstable.ansible-lint
+    sshpass
 
-      # VMs
-      quickemu
-      vagrant
+    # VMs
+    quickemu
+    vagrant
 
-      # distrobox
-      unstable.distrobox
+    # distrobox
+    unstable.distrobox
 
-      # kubernetes
-      unstable.minikube
-      unstable.kube3d
-      unstable.kind
+    # kubernetes
+    unstable.minikube
+    unstable.kube3d
+    unstable.kind
 
-      # kubernetes helm with plugins, helmfile & kompose
-      unstable.kompose
-      # (unstable.wrapHelm unstable.kubernetes-helm {
-      #   plugins = [
-      #     unstable.kubernetes-helmPlugins.helm-secrets
-      #     unstable.kubernetes-helmPlugins.helm-diff
-      #     unstable.kubernetes-helmPlugins.helm-s3
-      #   ];
-      # })
-      # unstable.helmfile
-      my-kubernetes-helm
-      my-helmfile
+    # kubernetes helm with plugins, helmfile & kompose
+    unstable.kompose
+    # (unstable.wrapHelm unstable.kubernetes-helm {
+    #   plugins = [
+    #     unstable.kubernetes-helmPlugins.helm-secrets
+    #     unstable.kubernetes-helmPlugins.helm-diff
+    #     unstable.kubernetes-helmPlugins.helm-s3
+    #   ];
+    # })
+    # unstable.helmfile
+    my-kubernetes-helm
+    my-helmfile
 
 
-      # kubernetes cli tools
-      unstable.argocd
-      unstable.kubectl
-      unstable.kubectx
-      unstable.kubecfg
-      unstable.kubie
-      unstable.k9s
-      unstable.stern
-    ];
+    # kubernetes cli tools
+    kubectl-v1-24-16
+    unstable.argocd
+    unstable.kubectx
+    unstable.kubecfg
+    unstable.kubie
+    unstable.k9s
+    unstable.stern
+  ];
 
 
   programs.go = {
