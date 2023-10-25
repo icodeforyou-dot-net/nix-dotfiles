e868067d12e1df6b9e3226f0c892c116951702dc
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-10-25 19:25:54 +0200

    Add insecure packages

--- scripts/apply-user.sh
+++ scripts/apply-user.sh
@@ -1,4 +1,4 @@
 export NIXPKGS_ALLOW_INSECURE=1
-nix build ../#homeConfigurations.${USERNAME}-${HOSTNAME}.activationPackage --show-trace
+nix build ../#homeConfigurations.${USERNAME}-${HOSTNAME}.activationPackage --show-trace --impure
 ./result/activate
 rm -rf ./result
