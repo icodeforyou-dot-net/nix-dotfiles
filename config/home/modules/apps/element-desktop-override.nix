8678287cdebf5997607b7c1a2a0955ebe804db59
icodeforyou-dot-net <andreas-p@tutanota.com>
2023-10-31 14:03:50 +0100

    Update element-desktop-override.nix

--- config/home/modules/apps/element-desktop-override.nix
+++ config/home/modules/apps/element-desktop-override.nix
@@ -4,12 +4,6 @@ let
   element-desktop-override = with pkgs; (unstable.element-desktop.overrideAttrs
     (oldAttrs: rec {
       pname = "element-desktop";
-      version = "3.118.22";
-      src = fetchurl {
-        url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/${pname}-${version}-unpacked-linux.tar.gz";
-        name = "tutanota-desktop-${version}.tar.gz";
-        sha256 = "hW3Jr0it2MKk0pDk4ytw1sixlAZ/6UhMg6slkmWvRag=";
-      };
     }));
 in
 
