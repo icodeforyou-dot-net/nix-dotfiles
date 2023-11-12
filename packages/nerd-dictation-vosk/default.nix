468812db817960bc2d90eeddaf3d5e94f82084ad
ap@icodeforyou.net <andreas-p@tutanota.com>
2023-11-12 20:54:45 +0100

    Cleanup

--- packages/nerd-dictation-vosk/default.nix
+++ packages/nerd-dictation-vosk/default.nix
@@ -1,17 +1,9 @@
 self: { pkgs, lib, stdenv, python3Packages, ... }@super:
 
 {
-
   nerd-dictation = pkgs.callPackage (import ./pkgs/nerd-dictation) { };
   vosk-api = pkgs.callPackage (import ./pkgs/vosk-api) { };
   kaldi = pkgs.callPackage (import ./pkgs/kaldi) { };
-
-  # vosk-python = pkgs.python3.callPackage (import ./pkgs/python-vosk) { };
-
-  # pythonOverrides = selfPython: superPython: {
-  #   vosk-python = selfPython.callPackage (import ./pkgs/python-vosk) { };
-  # };
-  # python3 = python3.pkgs.override { packageOverrides = pythonOverrides; };
 
   python3Vosk = pkgs.python3.override {
     packageOverrides = self: super: {
