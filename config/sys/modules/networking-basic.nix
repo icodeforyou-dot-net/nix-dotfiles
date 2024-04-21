e22b19b521f3e025fe4ec723546574b9ebd2ea79
icodeforyou-dot-net <andreas-p@tutanota.com>
2024-04-21 18:47:05 +0200

    refactor: move docker.internal hosts to different location

--- config/sys/modules/networking-basic.nix
+++ config/sys/modules/networking-basic.nix
@@ -4,6 +4,7 @@
     firewall.enable = true;
     hosts = {
       "192.168.200.100" = [ "minikube.local" ];
+      "127.0.0.1" = [ "host.docker.internal" ];
     };
     networkmanager.enable = true;
   };
