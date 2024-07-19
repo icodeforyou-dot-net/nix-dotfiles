d8175b3e416c2a75e1c20a7886a4946e8a53310b
ap@icodeforyou.net <andreas-p@tutanota.com>
2024-07-19 18:22:23 +0200

    feat: add github login script for private repos

---
+++ config/home/modules/scripts/default.nix
@@ -0,0 +1,5 @@
+{
+  imports = [
+    "./gh-login.sh"
+  ];
+}
--- config/hosts/cadmus-home.nix
+++ config/hosts/cadmus-home.nix
@@ -4,8 +4,7 @@
   imports = [
     ../home/modules/apps/element-desktop.nix
     ../home/modules/apps/alacritty.nix
-    ../home/modules/eww/eww.nix
-    ../home/modules/mako/mako.nix
+    ../home/modules/scripts
     ../home/modules/cli-os.nix
     ../home/modules/editors.nix
     # ../home/modules/fonts.nix
@@ -13,7 +12,6 @@
     ../home/modules/gui.nix
     ../home/modules/home-manager.nix
     ../home/modules/languages.nix
-    ../home/modules/swaylock.nix
     ../home/modules/wayland.nix
     #../home/modules/xdg.nix
     ../home/modules/xresources.nix
